f74e5cf33484c73dc3556571e76f363662a739d5
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-05-23 00:26:31 +0600

    chore: production build

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ echo "Starting script..."
 
 # Production build
 echo "Building production files..."
-ng build --prod --output-path docs --base-href /agency-landing-page/
+ng build --prod --output-path docs --base-href /angular-pdf-viewer/
 
 # Creating 404.html
 echo "Creating 404.html"
 cd docs
-sed -i "s/\(C:\/Program\sFiles\/Git\/agency-landing-page\/\)/\/agency-landing-page\//" index.html
+sed -i "s/\(C:\/Program\sFiles\/Git\/angular-pdf-viewer\/\)/\/angular-pdf-viewer\//" index.html
 cp index.html 404.html
 
 # Publishing to GitHub
